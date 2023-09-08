06968f1487bc48733c206b284a97f7ef21b414e5
AthrvaNaik <141731433+AthrvaNaik@users.noreply.github.com>
2023-09-08 23:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// ***********nav-menu**********
+const openMenu = ()=>{
+    document.querySelector('.backdrop').className='backdrop active';
+    document.querySelector('aside').className='active';
+}
+
+const closeMenu = ()=>{
+    document.querySelector('.backdrop').className='backdrop ';
+    document.querySelector('aside').className='';
+}
+
+document.getElementById("menuBtn").onclick = e =>{
+    e.preventDefault()
+    openMenu()
+}
+
+
+document.querySelector('aside button.close').onclick= e =>{
+    closeMenu()
+}
+
+document.querySelector('.backdrop').onclick=e=>{
+    closeMenu()
+}
+
+
+//***********scroll event*************
+document.lastScrollPosition=0
+document.lastCentered=0
+document.onWayTo=null
+document.addEventListener('scroll',()=>{
+    const direction=window.pageYOffset-document.lastScrollPosition >0?'down':'up'
+    const sections=[...document.querySelectorAll('section')]
+    if(document.onWayTo===null){
+        const destIndex=direction==='up'?document.lastCentered-1:document.lastCentered+1
+        if(destIndex>=0 && destIndex<sections.length){
+        document.onWayTo=destIndex
+        window.scroll(0,sections[destIndex].offsetTop)
+        }
+    }
+    
+    sections.forEach((section,index)=>{
+        if(window.pageYOffset ===section.offsetTop){
+            document.lastCentered=index;
+            section.className='active'
+            if(document.onWayTo===index){
+                document.onWayTo=null
+            }
+        }else{
+            section.className=''
+        }
+    })
+    document.lastScrollPosition=window.pageYOffset
+
+})
